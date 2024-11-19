8195833a598ee7beeed0a600b25658a354959851
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2024-11-19 12:39:07 -0500

    Update streamlit-columns1.py

--- streamlit-columns1.py
+++ streamlit-columns1.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = 'https://storage.googleapis.com/scsu-data-science/tips.csv'
+url = 'https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/tips.csv'
 df = pd.read_csv(url)
 
 st.title('Restaurant spending')
